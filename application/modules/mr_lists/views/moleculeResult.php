16875563d5a9012e8c3786c74d0d15033249dd22
Mukunda Vishwakarma <mukunda@techizer.in>
2019-09-25 19:24:28 +0530

    FIX :: Minor Changes in MR Lists

--- application/modules/mr_lists/views/moleculeResult.php
+++ application/modules/mr_lists/views/moleculeResult.php
@@ -1,8 +1,12 @@
 <table class="table table-striped table-condensed">
+    <th>Molecule</th>
+    <th>Brand Name</th>
+    <th>Other Brand Name</th>
+    <th>Rxn</th>
     <?php foreach($records as $record) : ?>
     <tr>
-        <td><?php echo $record['brand_name'] ?></td>
         <td><?php echo $record['molecule_name'] ?></td>        
+        <td><?php echo $record['brand_name'] ?></td>
         <td><?php echo $record['custom_brand_name'] ?></td>
         <td><?php echo $record['rxn'] ?></td>
     </tr>
